7a06a04d7e38562be57d08deb451ef07898d8cd2
Jasmine John <jsjohn026@gmail.com>
2025-02-21 15:55:02 -0600

    added index route to server file

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
-const MongoClient = require('mongodb').MongoClient
-require('dotenv').config()
+const connectDB = require('./config/database')
+require('dotenv').config({path: './config/.env'})
 const PORT = process.env.PORT
+const homeRoutes = require('./routes/home')
 
-let db, 
-    dbConnectionStr = process.env.DB_STRING
-    dbName = 'orders'
+connectDB()
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/', homeRoutes)
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on ${PORT}`)
+})
