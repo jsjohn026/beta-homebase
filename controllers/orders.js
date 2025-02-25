1ca0955ccaa677f167a99996a34bce20af7b106e
Jasmine John <jsjohn026@gmail.com>
2025-02-25 09:00:37 -0600

    Added ejs logic, post route, and createOrder controller to create work orders

--- controllers/orders.js
+++ controllers/orders.js
@@ -7,8 +7,17 @@ module.exports = {
         const ordersLeft = await Order.countDocuments({completed: false})
         res.render('orders.ejs', {orders: workOrders, left: ordersLeft})
     } catch (error) {
-      console.error(error)
+      console.error("Unable to load orders page:" + error)
     }
   },
-  
+
+  createOrder: async (req, res) => {
+    try {
+      await Order.create({order: req.body.workOrder, completed: false})
+      console.log('Work order has been added!')
+      res.redirect('/orders')
+    } catch (error) {
+      console.error("Unable to submit work order:" + error)
+    }
+  }
 }
--- routes/orders.js
+++ routes/orders.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const ordersController = require('../controllers/orders')
 
 router.get('/', ordersController.getOrders)
+router.post('/createOrder', ordersController.createOrder)
 
 module.exports = router
